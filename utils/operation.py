6ed761a2eb43a589739e253124c2de20428a0aad
WGXT <1468417901@qq.com>
2023-06-04 23:20:05 +0800

    完成ONNXModel类的开发

--- utils/operation.py
+++ utils/operation.py
@@ -5,3 +5,66 @@ import numpy as np
 from PIL import Image
 
 from utils.orientation import non_max_suppression, tag_images
+
+class ONNXModel(object):
+    def __init__(self, onnx_path):
+        """
+        输入参数：onnx_path onnx模型的路径
+        """
+        self.onnx_session = onnxruntime.InferenceSession(onnx_path)
+        self.input_name = self.get_input_name(self.onnx_session)
+        self.output_name = self.get_output_name(self.onnx_session)
+        # print("input_name:{}".format(self.input_name))
+        # print("output_name:{}".format(self.output_name))
+
+    def get_output_name(self, onnx_session):
+        """
+        遍历ONNX模型输出节点列表并返回一个包含所有节点名称的列表
+        """
+        output_name = []
+        for node in onnx_session.get_outputs():
+            output_name.append(node.name)
+        return output_name
+
+    def get_input_name(self, onnx_session):
+        """
+        遍历ONNX模型输入节点列表并返回一个包含所有节点名称的列表
+        """
+        input_name = []
+        for node in onnx_session.get_inputs():
+            input_name.append(node.name)
+        return input_name
+    
+    def get_input_feed(self, input_name, image_numpy):
+        input_feed = {}
+        for name in input_name:
+            input_feed[name] = image_numpy
+        return input_feed
+
+    def to_numpy(self, file, shape, gray=False):
+        if isinstance(file, np.ndarray):
+            img = Image.fromarray(file)
+        elif isinstance(file, bytes):
+            img = Image.open(BytesIO(file))
+            pass
+        else:
+            img = Image.open(file)
+
+        widht, hight = shape
+         # 改变大小 并保证其不失真
+        img = img.convert('RGB')
+        if gray:
+            img = img.convert('L')
+        img = img.resize((widht, hight), Image.ANTIALIAS)
+
+        # 转换成矩阵
+        image_numpy = np.array(img) # (widht, hight, 3)
+        if gray:
+            image_numpy = np.expand_dims(image_numpy,0)
+            image_numpy = image_numpy.transpose(0, 1, 2)
+        else:
+            image_numpy = image_numpy.transpose(2,0,1) # 转置 (3, widht, hight)
+        image_numpy = np.expand_dims(image_numpy,0)
+        # 数据归一化
+        image_numpy = image_numpy.astype(np.float32) / 255.0
+        return image_numpy
