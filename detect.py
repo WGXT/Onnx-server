5cb02778c15c05223e86d5691fef61db96393aa2
WGXT <1468417901@qq.com>
2023-06-07 01:09:33 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,12 +1,12 @@
 from PIL import Image,ImageDraw
 from utils.operation import YOLO
 
-def detect(onnx_path='ReqFile/yolov5n-7-k5.onnx',img_path='ReqFile/bus.jpg',show=True):
+def detect(onnx_path='Onnx-Model/detect-Occ.onnx',img_path='img/demo.jpg',show=True):
     '''
     检测目标，返回目标所在坐标如：
     {'crop': [57, 390, 207, 882], 'classes': 'person'},...]
-    :param onnx_path:onnx模型路径
-    :param img:检测用的图片
+    :param onnx_path:onnx:模型路径
+    :param img_path:检测用的图片
     :param show:是否展示
     :return:
     '''
@@ -16,7 +16,7 @@ def detect(onnx_path='ReqFile/yolov5n-7-k5.onnx',img_path='ReqFile/bus.jpg',show
     # 检测结果
     print (det_obj)
 
-    # 画出检测框框
+    # 画出检测框框(如果需要)
     if show:
         img = Image.open(img_path)
         draw = ImageDraw.Draw(img)
@@ -24,6 +24,7 @@ def detect(onnx_path='ReqFile/yolov5n-7-k5.onnx',img_path='ReqFile/bus.jpg',show
         for i in range(len(det_obj)):
             draw.rectangle(det_obj[i]['crop'],width=3)
         img.show()  # 展示
+
     return det_obj
 
 if __name__ == "__main__":
